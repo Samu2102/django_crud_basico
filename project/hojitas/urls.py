c3edca0471f4efdc036e3ce61a146bea6015b873
twtlorenaardila-png <twtlorenaardila@gmail.com>
2026-05-28 09:52:37 -0500

    project complete

--- project/hojitas/urls.py
+++ project/hojitas/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path #acceder a las urls
 from . import views #acceder a las vistas
+
+from django.conf import settings
+from django.conf.urls.static import static # se utilizarán archivos estáticos
+
 urlpatterns = [
     path('',views.inicio,name='inicio'),
 
@@ -7,4 +11,6 @@ urlpatterns = [
 #en la primera parte, nosotros vamos a escribir en la url la palabra nosotros. Accediendo a views.nosotros para mostrarlo.
     path('hojas/',views.hojas, name='hojas'), #la funcion es la de views.hojas
     path('hojas/crear',views.crearh, name='crearh'), #no se repita la url de hojas, se agrega hojas/crear
-]
+    path('editar/<int:id>',views.editarh, name='editarh'),
+    path('eliminar/<int:id>',views.eliminarh, name='eliminarh'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/hojitas/views.py
+++ project/hojitas/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse #HttpResponse sirve para enviar una respuesta HTTP al navegador desde una vista (views.py).
-
+from .models import Hoja
+from .forms import HojaForm
 # Create your views here.
 def inicio(request): #request es la solicitud a la aplicación
     return render(request, 'paginas/inicio.html')
@@ -11,9 +12,32 @@ def nosotros(request):
 # se direcciona /
 
 def hojas(request):
-    return render(request, 'hojas/index.html')
+#return render(request, 'hojas/index.html')
+    hojas=Hoja.objects.all()
+    return render(request, 'hojas/index.html',{'hojas':hojas})
 
 def crearh(request):
-    return render(request, 'hojas/crearh.html')
+    #return render(request, 'hojas/crearh.html')
+    formulario=HojaForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('hojas')
+    return render(request, 'hojas/crearh.html',{'formulario':formulario})
+#eso quiere decir que se le va enviar informacion al formulario de crearh.html a traves de la variable formulario
+
+
+def editarh(request,id): #se agrega el parametro que necesitamos.
+    hoja=Hoja.objects.get(id=id) #para que muestre los datos
+    formulario=HojaForm(request.POST or None, request.FILES or None,
+instance=hoja)
+    if formulario.is_valid() and request.POST: #o request.POST
+        formulario.save()
+        return redirect('hojas')
+    return render(request, 'hojas/editarh.html',{'formulario':formulario})
+
+def eliminarh(request, id):
+    hoja=Hoja.objects.get(id=id)
+    hoja.delete()
+    return redirect('hojas') #no hay una vista de eliminar, por lo que se redirecciona y queremos seguir en la vista de hojas porque allí esta el boton borrar.
 
     
